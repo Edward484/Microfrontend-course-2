545c0654b76689a2be4f68e2c86f5587780e92f4
Edward484 <selea.eduard@gmail.com>
2022-07-18 14:50:30 +0300

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from "./App";
+
+
+//mount function to start up the app
+const mount = (el) => {
+    ReactDom.render(
+        <App/>,
+        el
+    );
+}
+
+//case application in development
+if (process.env.NODE_ENV === 'development'){
+    const el = document.querySelector('#_marketing-dev-root');
+    if(el){
+        mount(el)
+    }
+}
+
+export {mount}
+
+
