9ee044615bed01d9d48c3fc41be94e91ffebacae
Edward484 <selea.eduard@gmail.com>
2022-07-19 17:34:27 +0300

    added router to child and parent and now they can communicate

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import {StylesProvider,createGenerateClassName} from "@material-ui/core";
-import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Router, Switch} from "react-router-dom";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 import React from 'react'
@@ -8,17 +8,17 @@ const generateClassName = createGenerateClassName({
     productionPrefix:'ma',
 })
 
-const App = () => {
+const App = ({history}) => {
 
     return (
         <div>
             <StylesProvider generateClassName={generateClassName}>
-                <BrowserRouter>
+                <Router history={history}>
                     <Switch>
                         <Route exact path={'/pricing'} component={Pricing}/>
                         <Route exact path={'/'} component={Landing}/>
                     </Switch>
-                </BrowserRouter>
+                </Router>
             </StylesProvider>
         </div>
     )
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,24 +1,39 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './App';
 
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
 
-//mount function to start up the app
-const mount = (el) => {
-    ReactDom.render(
-        <App/>,
-        el
-    );
-}
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
 
-//case application in development
-if (process.env.NODE_ENV === 'development'){
-    const el = document.querySelector('#_marketing-dev-root');
-    if(el){
-        mount(el)
+    ReactDOM.render(<App history={history} />, el);
+
+    return{
+      onParentNavigate({pathname : nextPathname}){
+          const {pathname} = history.location
+
+          if (pathname !== nextPathname){
+              history.push(nextPathname)
+          }
+      }
+    };
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot, {defaultHistory:createBrowserHistory() });
     }
 }
 
-export {mount}
-
-
+// We are running through container
+// and we should export the mount function
+export { mount };
