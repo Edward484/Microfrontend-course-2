8bc21c00915cde513d905b4c266715b4b3376686
Edward484 <selea.eduard@gmail.com>
2022-07-19 15:34:49 +0300

    added randomized production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import {BrowserRouter} from "react-router-dom";
+import {createGenerateClassName, StylesProvider} from "@material-ui/core";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+})
 
 const App = () => {
 
     return (
         <BrowserRouter>
-            <div>
-                <Header/>
-                <MarketingApp/>
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header/>
+                    <MarketingApp/>
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,14 +1,18 @@
-import {StylesProvider} from "@material-ui/core";
+import {StylesProvider,createGenerateClassName} from "@material-ui/core";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing";
 import React from 'react'
 
+const generateClassName = createGenerateClassName({
+    productionPrefix:'ma',
+})
+
 const App = () => {
 
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path={'/pricing'} component={Pricing}/>
