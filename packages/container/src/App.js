a56e99f119fdf39da71aeddb344a59352989a1ad
Edward484 <selea.eduard@gmail.com>
2022-07-20 14:48:09 +0300

    added auth page added auth logic with classbacks resolved routes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import MarketingApp from "./components/MarketingApp";
+import React, {lazy, Suspense, useState} from 'react'
 import Header from "./components/Header";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
 })
 
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+
 const App = () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
 
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header/>
-                    <MarketingApp/>
+                    <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)}/>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path={"/auth"}>
+                                <AuthLazy onSignIn={() => setIsSignedIn(true)}/>
+                            </Route>
+                            <Route path={"/"}>
+                                <MarketingLazy/>
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </div>
             </StylesProvider>
         </BrowserRouter>
